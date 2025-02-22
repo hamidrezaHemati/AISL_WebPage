57a3ef6d2fbdfc0fbe74d5199bcdd02f18ca2e02
unknown <hrhhemati@gmail.com>
2025-02-22 21:00:30 +0330

    Updating Home page, adding some pages

---
+++ my-website/src/pages/Research.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>Researchs</h1>
+      <p>Researchs caried out in this Laboratory</p>
+    </Layout>
+  );
+}
