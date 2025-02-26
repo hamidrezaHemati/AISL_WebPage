bdc52b823911db587eb1f9b391dcbd25068ac23b
unknown <hrhhemati@gmail.com>
2025-02-26 20:41:06 +0330

    People page template completed

--- my-website/src/pages/people.js
+++ my-website/src/pages/people.js
@@ -7,14 +7,28 @@ export default function PeoplePage() {
     <Layout>
       <div className="people-container">
         {/* Lab Members Grid */}
-        <div className="prof-grid" id="professors">
-          {Prof.map((item, index) => (
-            <div key={index} className="prof-card">
-              <img src={item.image} alt={item.title} />
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
+
+        <div className="prof-card" id="prof">
+          <div className="prof-img-div">
+            <img src="/People/Dr_Javanmardi.jpg" alt="professor img" className="prof-img"/>
           </div>
-          ))}
+          <div className="prof-content">
+            <div className="prof-content-div">
+              <h2 className="prof-title">Dr. Mahdi Javanmardi</h2>
+            </div>
+            <div className="prof-content-div">
+              <p className="prof-position">Assistant Professor, Amirkabir University of Technology</p>
+            </div>
+            <div className="prof-content-div">
+              <p className="prof-lab-state">AISL director</p>
+            </div>
+            <div className="prof-content-div">
+              <p className="prof-descripton">Mahdi Javanmardi (Member, IEEE) received the M.Sc. degree from the <b>Sharif University of Technology</b>, Iran, in 2013, and the Ph.D. degree in information and communication engineering from The <b>University of Tokyo</b>, Japan, in 2017.,He was a Visiting Student Researcher with the <b>University of California, Berkeley</b>, from 2016 to 2017. He has also completed the Graduate Program for Social ICT Global Creative Leaders with The University of Tokyo, in 2017. He has been a Post-Doctoral Researcher with the Institute of Industrial Science, The University of Tokyo, since 2017. His research interests include localization and mapping for an autonomous vehicle, autonomous vehicle’s perception, and computer vision. He is a member of the IEEE ITS Society..</p>
+            </div>
+            <div className="prof-content-div">
+              <p className="prof-contact">Email: <a>mjavan@aut.ac.ir</a></p>
+            </div>
+          </div>
         </div>
 
         <h1 className="styled-heading">Research Assistants</h1>
@@ -24,28 +38,31 @@ export default function PeoplePage() {
               <img src={item.image} alt={item.title} />
               <h3>{item.title}</h3>
               <p>{item.description}</p>
+              <p>Email: <a>{item.email}</a></p>
           </div>
           ))}
         </div>
 
-        <h1 className="styled-heading">Masters' Students</h1>
+        <h1 className="styled-heading">Master Students</h1>
         <div className="people-grid" id="master-members">
           {MasterStudents.map((item, index) => (
             <div key={index} className="people-card">
               <img src={item.image} alt={item.title} />
               <h3>{item.title}</h3>
               <p>{item.description}</p>
+              <p>Email: <a>{item.email}</a></p>
           </div>
           ))}
         </div>
 
-        <h1 className="styled-heading">Bachelors' Students</h1>
+        <h1 className="styled-heading">Bachelor Students</h1>
         <div className="people-grid" id="bachelor-members">
           {BachelorStudents.map((item, index) => (
             <div key={index} className="people-card">
               <img src={item.image} alt={item.title} />
               <h3>{item.title}</h3>
               <p>{item.description}</p>
+              <p>Email: <a>{item.email}</a></p>
           </div>
           ))}
         </div>
@@ -57,6 +74,7 @@ export default function PeoplePage() {
               <img src={item.image} alt={item.title} />
               <h3>{item.title}</h3>
               <p>{item.description}</p>
+              <p>Email: <a>{item.email}</a></p>
           </div>
           ))}
         </div>
@@ -65,32 +83,28 @@ export default function PeoplePage() {
   );
 }
 
-const Prof = [
-  { image: "/People/Dr_Javanmardi.jpg", title: "Dr. Mahdi Javanmardi", description: "Assistant Proffesor. AISL director", position: "Professor" }
-];
-
 const Researchers = [
-  { image: "/People/Hamidreza Hemati.jpg", title: "Hamidreza Hemati", description: "Researcher in AI and robotics.", position: "Researcher" },
-  { image: "/People/Ayar.jpg", title: "Fardin Ayar", description: "Researcher in AI and robotics.", position: "Researcher" },
-  { image: "/People/Amir Kashi.jpg", title: "Amirhosein Kashani", description: "Researcher in AI and robotics.", position: "Researcher" },
-  { image: "/People/Talebi.jpg", title: "Mitra Talebi", description: "Develops real-time control systems.", position: "Master's Student" }
+  { image: "/People/Hamidreza Hemati.jpg", title: "Hamidreza Hemati", description: "Researcher in AI and robotics.", position: "Researcher", email: "hrh.hemati@gmail.com" },
+  { image: "/People/Ayar.jpg", title: "Fardin Ayar", description: "Researcher in AI and robotics.", position: "Researcher", email: "-"  },
+  { image: "/People/Amir Kashi.jpg", title: "Amirhosein Kashani", description: "Researcher in AI and robotics.", position: "Researcher", email: "-"  },
+  { image: "/People/Talebi.jpg", title: "Mitra Talebi", description: "Develops real-time control systems.", position: "Master's Student", email: "-"  }
 ];
 
 const MasterStudents = [
-  { image: "/People/Dolatabadi.jpg", title: "Minoo Dolatabadi", description: "Expert in sensor fusion.", position: "Researcher" },
-  { image: "/People/Gholinejhad.jpg", title: "Ghazaleh Gholinejhad", description: "Works on deep learning applications.", position: "Researcher" },
-  { image: "/People/Eftekhar.jpg", title: "Mohamad Sadegh Eftekhar", description: "VLM-based control of Autonomous vehicles", position: "Researcher" },
+  { image: "/People/Dolatabadi.jpg", title: "Minoo Dolatabadi", description: "Expert in sensor fusion.", position: "Researcher", email: "-"  },
+  { image: "/People/Gholinejhad.jpg", title: "Ghazaleh Gholinejhad", description: "Works on deep learning applications.", position: "Researcher", email: "-"  },
+  { image: "/People/Eftekhar.jpg", title: "Mohamad Sadegh Eftekhar", description: "VLM-based control of Autonomous vehicles", position: "Researcher", email: "-"  },
   ];
 
 const BachelorStudents = [
-  { image: "/People/Hamdi.jpg", title: "Daniyal Hamdi", description: "Developer of autonomous vehicle simulations.", position: "Researcher" },
-  { image: "/People/hemmasian.jpg", title: "AmirParsa Hemmasian", description: "Specializes in wireless communications.", position: "Researcher" },
-  { image: "/People/kabiri.jpg", title: "Abolfazl Kabiri", description: "Researcher in IoT security.", position: "Researcher" },
-  { image: "/People/Mobli2.jpg", title: "Aida Mobli", description: "Visual SLAM", position: "Researcher" },
+  { image: "/People/Hamdi.jpg", title: "Daniyal Hamdi", description: "Developer of autonomous vehicle simulations.", position: "Researcher", email: "-"  },
+  { image: "/People/hemmasian.jpg", title: "AmirParsa Hemmasian", description: "Specializes in wireless communications.", position: "Researcher", email: "-"  },
+  { image: "/People/kabiri.jpg", title: "Abolfazl Kabiri", description: "Researcher in IoT security.", position: "Researcher", email: "-"  },
+  { image: "/People/Mobli2.jpg", title: "Aida Mobli", description: "Visual SLAM", position: "Researcher", email: "-"  },
   ];
 
 const Alumni = [
-  { image: "/People/mohammad bagheri.jpg", title: "Mohammad Bagheri", description: "Works on reinforcement learning.", position: "Researcher" },
-  { image: "/People/Rezaee.jpg", title: " Mohamad Ali Rezaee", description: "Expert in cloud computing.", position: "Researcher" },
-  { image: "/People/ardakanian.jpg", title: "Bardia Ardakanian", description: "PhD student in embedded systems.", position: "PhD Student" },
+  { image: "/People/mohammad bagheri.jpg", title: "Mohammad Bagheri", description: "Works on reinforcement learning.", position: "Researcher", email: "-"  },
+  { image: "/People/Rezaee.jpg", title: " Mohamad Ali Rezaee", description: "Expert in cloud computing.", position: "Researcher", email: "-"  },
+  { image: "/People/ardakanian.jpg", title: "Bardia Ardakanian", description: "PhD student in embedded systems.", position: "PhD Student", email: "-"  },
 ];
